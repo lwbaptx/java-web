d6bb97221902cbb59e1c70d107e29b499606305b
lwbaptx <lwbaptx@gmail.com>
2019-02-10 19:59:30 +0800

    beer-v1 commit

---
+++ beer-v1/src/com/example/web/BeerSelect.java
@@ -0,0 +1,28 @@
+package com.example.web;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.util.Iterator;
+import java.util.List;
+
+import com.example.model.*;
+
+public class BeerSelect extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String c = request.getParameter("color");
+        BeerExpert be = new BeerExpert();
+        List result = be.getBrands(c);
+        //response.setContentType("text/html");
+        //PrintWriter out = response.getWriter();
+        //out.println("Beer Selection Advice<br>");
+        //out.println("<br>Got beer color " + c);
+        //Iterator it = result.iterator();
+        //while(it.hasNext()) {
+        //    out.print("<br>try: " + it.next());
+        //}
+        request.setAttribute("styles", result);
+        RequestDispatcher view = request.getRequestDispatcher("result.jsp");
+        view.forward(request, response);
+    }
+}
